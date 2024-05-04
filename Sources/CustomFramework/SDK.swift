0b2b14494cf8e148acb76fb0b3a2bc8266ddd3e2
kymo7 <kmoon35@icloud.com>
2024-05-04 14:02:57 -0700

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Kyle Mooney on 5/4/24.
+//
+
+import Foundation
+
+struct SDK {
+    static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
